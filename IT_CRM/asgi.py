0f9759d87e9912c34eade7d7eede9c349ac46c3a
looorker <looorker@gmail.com>
2023-12-15 06:52:01 -0700

    Start config for project

---
+++ IT_CRM/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for IT_CRM project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IT_CRM.settings')
+
+application = get_asgi_application()
